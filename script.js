edc85e52c5a602517bfced90c7fae64944f06a64
Leonardo Aldana <112524903+compuleo@users.noreply.github.com>
2023-10-05 20:37:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert("Hola mi gato! Miauuuu");
+
+let nombre = "Leonardo";
+
+var nombre1 = "Santi";
+
+const nombre2 = "Sandra";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+alert(nombre + nombre1 + nombre2);
